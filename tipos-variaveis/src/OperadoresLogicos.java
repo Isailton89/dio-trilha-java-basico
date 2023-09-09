24dd288043c3540cb3f469319aa7418192bc665f
Isailton89 <isailton2008@gmail.com>
2023-09-09 15:04:43 -0300

    adicionando as aulas de Operados

---
+++ tipos-variaveis/src/OperadoresLogicos.java
@@ -0,0 +1,16 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }
+        
+        if(condicao1 || condicao2){
+            System.out.println("Uma das duas condições são verdadeiras!");
+        }
+
+        System.out.println("Final das avaliações");
+    }
+}
