9d760176e0405a71fbdbb8cc4e64ce0b87e7e03d
Isailton89 <isailton2008@gmail.com>
2023-09-10 19:30:09 -0300

    inserindo arquivos das aulas de métodos

--- tipos-variaveis/src/Usuario.java
+++ tipos-variaveis/src/Usuario.java
@@ -2,8 +2,22 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
+        smartTv.abaixarVolume();
+        smartTv.abaixarVolume();
+        smartTv.abaixarVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
         System.out.println("A TV está ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
     }
 }
